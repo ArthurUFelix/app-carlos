86b2189d9331b11a2496fd16903fecf1ca30825b
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2020-11-09 18:49:59 -0300

    update routes

--- src/routes.js
+++ src/routes.js
@@ -16,8 +16,6 @@ routes.post('/session', wrap(SessionController.store))
 routes.post('/company', wrap(CompanyController.store))
 routes.get('/company/:companyId', wrap(CompanyController.get))
 
-routes.get('/queue/:queueId', wrap(QueueController.get))
-
 routes.get('/user/:userId', wrap(UserController.get))
 routes.get('/user/queue/:queueId', wrap(QueueController.listQueueUsers))
 routes.post('/user', wrap(UserController.store))
@@ -31,6 +29,7 @@ routes.put('/company/:companyId', wrap(CompanyController.update))
 routes.delete('/company/:companyId', wrap(CompanyController.remove))
 
 routes.get('/queue', wrap(QueueController.listAll))
+routes.get('/queue/:queueId', wrap(QueueController.get))
 routes.post('/queue', wrap(QueueController.store))
 routes.put('/queue/:queueId', wrap(QueueController.update))
 routes.delete('/queue/:queueId', wrap(QueueController.remove))
