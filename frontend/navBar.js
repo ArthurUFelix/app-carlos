43a383981ae344dced559b54f6626fceb2f8dbc0
Pamylla <pamyllaostermann@gmail.com>
2020-11-30 21:51:50 -0300

    Adicionado modulo de suporte FRESHCHAT

--- frontend/navBar.js
+++ frontend/navBar.js
@@ -87,3 +87,12 @@ const logout = (event) => {
   localStorage.clear("company");
   window.location.href = "./companyLogin.html";
 };
+
+function initFreshChat() {
+    window.fcWidget.init({
+      token: "eea69b6d-f878-4ba9-83e5-1993b78690a4",
+      host: "https://wchat.freshchat.com"
+    });
+  }
+
+  function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
